0669c07c27cb867cafb373e05ef403181a997f2e
Tokiniaina <tokiniaina090605@gmail.com>
2025-06-20 16:59:36 +0300

    fix: implement crud in todo

--- todo/src/app.module.ts
+++ todo/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- todo/src/todo/todo.service.spec.ts
+++ todo/src/todo/todo.service.spec.ts
@@ -4,6 +4,13 @@ import { TodoService } from './todo.service';
 describe('TodoService', () => {
   let service: TodoService;
 
+  // const todoRepository = {
+  //   save: jest.fn(),
+  //   find: jest.fn(),
+  //   findOneBy: jest.fn(),
+  //   delete: jest.fn(),
+  // };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [TodoService],
@@ -14,5 +21,18 @@ describe('TodoService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a todo', async () => {
+    const todo = await service.create({ title: 'Test' });
+    expect(todo).toBeDefined();
+    expect(todo.title).toBe('Test');
+    expect(todo.done).toBe(false);
+  });
+
+  it('should find all todos', async () => {
+    const todos = await service.findAll();
+    expect(todos).toBeDefined();
+    expect(todos.length).toBe(1);
   });
 });
